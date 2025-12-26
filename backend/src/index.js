59d9435920773b59924781d1a1dc7967838e4110
shivansh0536 <shivansh050906@gmail.com>
2025-12-27 01:46:53 +0530

    commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,8 @@
-const app = require('./app');
-const dotenv = require('dotenv');
+if (process.env.NODE_ENV !== 'production' || !process.env.VERCEL) {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
 
-dotenv.config();
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
